820b2c2b80ad6c81648e74624d2095796c9b024f
Pong Loong Yeat <lpon0001@student.monash.edu>
2019-07-09 22:43:00 +1000

    Removed unnecessary dependencies

--- sign_detection.py
+++ sign_detection.py
@@ -1,12 +1,15 @@
-# Written by: Pong Loong Yeat
-# Last modified: 15/1/2019
+# Written by: Pong Loong Yeat, Calvin Low
+# Last modified: 16/1/2019
 # Dependencies: misc.py, rectangle_detection.py, text_recognition.py
 
 from rectangle_detection import detect_rect
 from text_recognition import read_text
 import time
-from misc import*
-from arrow_detection import*
+from misc import *
+from picamera import PiCamera
+from time import sleep
+import subprocess
+from arrow_detection import *
 
 
 ##################################################
@@ -41,6 +44,13 @@ def detect_sign(img_path):
 
     return arrow, directories
 
+
+def img_capture(img_path):
+    src_path = "/home/pi/indoor_navigation/summer_2019/"
+    cmd = "raspistill -q 100 -md 2 -vf -hf -o " + src_path + img_path
+    subprocess.call(cmd, shell=True)
+
+
 def process_directories(img_path, chosen_dir):
 
     if not isinstance(img_path, str):
@@ -60,24 +70,37 @@ def process_directories(img_path, chosen_dir):
     end = time.time()
     print("[INFO] process took {:.6f} seconds".format(end - start))
 
-    return i, len(directories)
+    return i
 
-def process_arrow(img_arrow, i, row):
 
+def process_arrow(img_arrow, i):
     start = time.time()
 
     height, width = img_arrow.shape[:2]
-    arrow_dir = separate_sign(img_arrow, width, height, row)
+    arrow_dir = separate_sign(img_arrow, width, height, 4)
 
     end = time.time()
     print("[INFO] process took {:.6f} seconds".format(end - start))
 
     return arrow_dir[i]
 
-src_path = "C:/Users/clair/Downloads/Summer Project/personal-projects/image-processing/images/"
+
+src_path = "/home/pi/indoor_navigation/summer_2019/images/"
 arrow_image, directories = detect_sign(src_path + "test4.jpg")
-index,row = process_directories("directories.png","LAB")
-arrow_direction = process_arrow(arrow_image,index)
+index = process_directories("directories.png", "LAB")
+arrow_direction = process_arrow(arrow_image, index)
 
 print(arrow_direction)
 
+# image_path = 'images/test4.jpg'
+# img_capture(image_path)
+# detect_sign(image_path)
+# src_path = "/home/pi/indoor_navigation/summer_2019/images/"
+# img_path = src_path + "warped.png"
+# img = cv2.imread(img_path)
+# height, width = img.shape[:2]
+# sign_pic,word_pic = cut_sign(img,width,height)
+#
+# sign_dir = separate_sign(sign_pic,width,height,4)
+# print(read_text("col_word.png"))
+# print(sign_dir)
